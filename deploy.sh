dbbd62c43b2ac6e35c1c8f523c6051c117eab710
appala+1-at-405778057295 <sriappal@cisco.com>
2019-09-22 16:16:20 -0700

    aal

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t ollekkam1/k8s/multi-client:latest -t ollekkam1/k8s/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ollekkam1/k8s/multi-server:latest -t ollekkam1/k8s/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ollekkam1/k8s/multi-worker:latest -t ollekkam1/k8s/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push ollekkam1/k8s/multi-client:latest
-docker push ollekkam1/k8s/multi-server:latest
-docker push ollekkam1/k8s/multi-worker:latest
-docker push ollekkam1/k8s/multi-client:$SHA
-docker push ollekkam1/k8s/multi-server:$SHA
-docker push ollekkam1/k8s/multi-worker:$SHA
+docker build -t ollekkam1/k8s:multi-client:latest -t ollekkam1/k8s:multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ollekkam1/k8s:multi-server:latest -t ollekkam1/k8s:multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ollekkam1/k8s:multi-worker:latest -t ollekkam1/k8s:multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ollekkam1/k8s:multi-client:latest
+docker push ollekkam1/k8s:multi-server:latest
+docker push ollekkam1/k8s:multi-worker:latest
+docker push ollekkam1/k8s:multi-client:$SHA
+docker push ollekkam1/k8s:multi-server:$SHA
+docker push ollekkam1/k8s:multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployment/server-deployment server=ollekkam1/k8s/multi-server:$SHA
-kubectl set image deployment/client-deployment client=ollekkam1/k8s/multi-client:$SHA
-kubectl set image deployment/worker-deployment worker=ollekkam1/k8s/multi-worker:$SHA
+kubectl set image deployment/server-deployment server=ollekkam1/k8s:multi-server:$SHA
+kubectl set image deployment/client-deployment client=ollekkam1/k8s:multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=ollekkam1/k8s:multi-worker:$SHA
 
